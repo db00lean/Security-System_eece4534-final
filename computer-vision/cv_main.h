7b257e1e61a34b02aeddf76dea3b8247ea10d53e
Jack <horton.r@husky.neu.edu>
2022-03-24 18:53:51 -0400

    Added comments

--- computer-vision/cv_main.h
+++ computer-vision/cv_main.h
@@ -7,6 +7,7 @@
 
 #define MAX_BBOXES 5
 
+// holds the data needed to draw an individual bounding box
 struct coordinate_data {
   uint16_t x_coord;
   uint16_t y_coord;
@@ -14,10 +15,11 @@ struct coordinate_data {
   uint16_t y_len;
 };
 
+// struct that will be transmitted and contains all necessary CV info
 struct cv_data {
-  int num_bbox = 0;
-  time_t t = time(0);
-  struct coordinate_data box_data[MAX_BBOXES];
+  int num_bbox = 0; // number of bounding boxes
+  time_t t = time(0); // timestamp (will be provided by camera team)
+  struct coordinate_data box_data[MAX_BBOXES]; // collection of bounding boxes
 };
 
 #endif
