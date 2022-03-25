d5e49cb9a05bcfa914f787ec42837d156bdc2856
Tom Doyle <doyle.t@northeastern.edu>
2022-03-25 17:00:29 -0400

    printing screen resolution

--- HDMI/display/fb/fb-test-app-master/boundingbox.c
+++ HDMI/display/fb/fb-test-app-master/boundingbox.c
@@ -6,46 +6,66 @@ uses drawpixel library to create a rectangle given user input
 */
 
 #include <stdio.h>
-#include "drawpixel.h"
+#include <linux/fb.h>
+#include <stdint.h>
 
-static struct fb_info fb_info;
+#include <sys/ioctl.h>
+#include <fcntl.h>
 
+//#include "drawpixel.h"
 
-void draw_boundingbox(int x_start, int y_start, int x_len, int y_len) {
-    //origin is top left of rectangle (x_start, y_start)
+//static struct fb_info fb_info;
+static struct fb_var_screeninfo screeninfo;
 
-    unsigned int color = 0xffffff;
-    int x, y;
+void fb_open(){
+    int fd;
+	fd = open("/dev/fb0", O_RDWR);
 
-    //draw top line
-    x = x_start;
-    y = y_start;
-    for (x = x_start; x < x_len + x_start; x++) {
-        draw_pixel(&fb_info, x, y, color);
+    if (fd == -1){
+        printf("bad file\n");
     }
-
-    //draw bottom line
-    x = x_start;
-    y = y_start + y_len;
-    for (x = x_start; x < x_len + x_start; x++) {
-        draw_pixel(&fb_info, x, y, color);
-    }
-
-    //draw left line
-    x = x_start;
-    y = y_start;
-    for (y = y_start; y < y_len + y_start; y++) {
-        draw_pixel(&fb_info, x, y, color);
-    }
-
-    //draw right line
-    x = x_start + x_len;
-    y = y_start;
-    for (y = y_start; y < y_len + y_start; y++) {
-        draw_pixel(&fb_info, x, y, color);
-    }
-
+	ioctl(fd, FBIOGET_VSCREENINFO, &screeninfo);
 }
+
+
+
+// void draw_boundingbox(int x_start, int y_start, int x_len, int y_len) {
+//     //origin is top left of rectangle (x_start, y_start)
+
+//     //white
+//     // unsigned int color = 0xffffff;
+//     unsigned int color = 0xff0000;
+//     int x, y;
+
+//     //draw top line
+//     x = x_start;
+//     y = y_start;
+//     for (x = x_start; x < x_len + x_start; x++) {
+//         draw_pixel(&fb_info, x, y, color);
+//     }
+
+//     //draw bottom line
+//     x = x_start;
+//     y = y_start + y_len;
+//     for (x = x_start; x < x_len + x_start; x++) {
+//         draw_pixel(&fb_info, x, y, color);
+//     }
+
+//     //draw left line
+//     x = x_start;
+//     y = y_start;
+//     for (y = y_start; y < y_len + y_start; y++) {
+//         draw_pixel(&fb_info, x, y, color);
+//     }
+
+//     //draw right line
+//     x = x_start + x_len;
+//     y = y_start;
+//     for (y = y_start; y < y_len + y_start; y++) {
+//         draw_pixel(&fb_info, x, y, color);
+//     }
+
+// }
 
 
 
@@ -56,32 +76,24 @@ int main() {
     int x_len = 100;
     int y_len = 100;
 
-/*    //read values x_start, y_start and x_len and y_len from sysfs
-    FILE *fp = fopen("/sys/class/axitimer/axitimer0/control/state", "r");
-    char* read_buffer;
-    size_t res = fread(read_buffer, 1, 1, fp);
-    //convert to int
-    x_start = atoi(read_buffer);
-    fclose(fp);
-
-
-    fp = fopen("/sys/class/axitimer/axitimer0/control/period", "r");
-    res = fread(read_buffer, 1, 1, fp);
-    //convert to int
-    y_start = atoi(read_buffer);
-    fclose(fp);
-
-    fp = fopen("/sys/class/axitimer/axitimer0/control/duty", "r");
-    res = fread(read_buffer, 1, 1, fp);
-    //convert to int
-    x_len = atoi(read_buffer);
-    y_len = atoi(read_buffer);
-    fclose(fp);
-*/
     //draw bounding box
-    fb_open(0, &fb_info);
+    fb_open();
 
-    draw_boundingbox(x_start, y_start, x_len, y_len);
+    uint32_t xRes = screeninfo.xres;
+    uint32_t yRes = screeninfo.yres;
+
+    uint32_t xRes_virtual = screeninfo.xres_virtual;
+    uint32_t yRes_virtual = screeninfo.yres_virtual;
+
+    uint32_t bits_per_pixel = screeninfo.bits_per_pixel;
+
+
+    printf("xres: %d, yres: %d\n", xRes, yRes);
+    printf("xres: %d, yres: %d\n", xRes_virtual, yRes_virtual);
+    printf("bits per pixel: %d\n", bits_per_pixel);
+
+
+    // draw_boundingbox(x_start, y_start, x_len, y_len);
 
 
     return 0;
