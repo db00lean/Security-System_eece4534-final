e726e76c92fae151e0297dad4c421cc63d24dba8
Tom Doyle <doyle.t@northeastern.edu>
2022-03-25 17:59:02 -0400

    drew rectangle all by ourselves

--- HDMI/display/fb/fb-test-app-master/boundingbox.c
+++ HDMI/display/fb/fb-test-app-master/boundingbox.c
@@ -10,12 +10,17 @@ uses drawpixel library to create a rectangle given user input
 #include <stdint.h>
 
 #include <sys/ioctl.h>
+#include <sys/mman.h>
+
 #include <fcntl.h>
 
 //#include "drawpixel.h"
 
 //static struct fb_info fb_info;
-static struct fb_var_screeninfo screeninfo;
+static struct fb_var_screeninfo var_screeninfo;
+static struct fb_fix_screeninfo fix_screeninfo;
+
+static void *fbMemPtr; 
 
 void fb_open(){
     int fd;
@@ -24,48 +29,71 @@ void fb_open(){
     if (fd == -1){
         printf("bad file\n");
     }
-	ioctl(fd, FBIOGET_VSCREENINFO, &screeninfo);
+	ioctl(fd, FBIOGET_VSCREENINFO, &var_screeninfo);
+	ioctl(fd, FBIOGET_FSCREENINFO, &fix_screeninfo);
+
+    
+    
+    fbMemPtr = mmap(0,
+			var_screeninfo.yres_virtual * fix_screeninfo.line_length,
+			PROT_WRITE | PROT_READ,
+			MAP_SHARED, fd, 0);
+    printf("fbmem pointer initial %x\n", fbMemPtr);
+
+}
+
+static void draw_pixel(int x, int y, uint32_t color)
+{
+    uint32_t *pixelPtr;
+    pixelPtr = fbMemPtr;
+    pixelPtr += fix_screeninfo.line_length * y;
+    pixelPtr += x;
+
+    // printf("fbMemPtr: 0x%x\n", fbMemPtr);
+    // printf("pixelPtr: 0x%x\n", pixelPtr);
+
+    // printf("x %d, y %d, color 0x%x\n", x, y, color);
+    *pixelPtr = color;
 }
 
 
+void draw_boundingbox(int x_start, int y_start, int x_len, int y_len) {
+    //origin is top left of rectangle (x_start, y_start)
 
-// void draw_boundingbox(int x_start, int y_start, int x_len, int y_len) {
-//     //origin is top left of rectangle (x_start, y_start)
+    //white
+    // unsigned int color = 0xffffff;
+    unsigned int color = 0x00ff00;
+    int x, y;
 
-//     //white
-//     // unsigned int color = 0xffffff;
-//     unsigned int color = 0xff0000;
-//     int x, y;
+    //draw top line
+    x = x_start;
+    y = y_start;
+    for (x = x_start; x < x_len + x_start; x++) {
+        draw_pixel(x, y, color);
+    }
 
-//     //draw top line
-//     x = x_start;
-//     y = y_start;
-//     for (x = x_start; x < x_len + x_start; x++) {
-//         draw_pixel(&fb_info, x, y, color);
-//     }
+    //draw bottom line
+    x = x_start;
+    y = y_start + y_len;
+    for (x = x_start; x < x_len + x_start; x++) {
+        draw_pixel(x, y, color);
+    }
 
-//     //draw bottom line
-//     x = x_start;
-//     y = y_start + y_len;
-//     for (x = x_start; x < x_len + x_start; x++) {
-//         draw_pixel(&fb_info, x, y, color);
-//     }
+    //draw left line
+    x = x_start;
+    y = y_start;
+    for (y = y_start; y < y_len + y_start; y++) {
+        draw_pixel(x, y, color);
+    }
 
-//     //draw left line
-//     x = x_start;
-//     y = y_start;
-//     for (y = y_start; y < y_len + y_start; y++) {
-//         draw_pixel(&fb_info, x, y, color);
-//     }
+    //draw right line
+    x = x_start + x_len;
+    y = y_start;
+    for (y = y_start; y < y_len + y_start; y++) {
+        draw_pixel(x, y, color);
+    }
 
-//     //draw right line
-//     x = x_start + x_len;
-//     y = y_start;
-//     for (y = y_start; y < y_len + y_start; y++) {
-//         draw_pixel(&fb_info, x, y, color);
-//     }
-
-// }
+}
 
 
 
@@ -79,13 +107,13 @@ int main() {
     //draw bounding box
     fb_open();
 
-    uint32_t xRes = screeninfo.xres;
-    uint32_t yRes = screeninfo.yres;
+    uint32_t xRes = var_screeninfo.xres;
+    uint32_t yRes = var_screeninfo.yres;
 
-    uint32_t xRes_virtual = screeninfo.xres_virtual;
-    uint32_t yRes_virtual = screeninfo.yres_virtual;
+    uint32_t xRes_virtual = var_screeninfo.xres_virtual;
+    uint32_t yRes_virtual = var_screeninfo.yres_virtual;
 
-    uint32_t bits_per_pixel = screeninfo.bits_per_pixel;
+    uint32_t bits_per_pixel = var_screeninfo.bits_per_pixel;
 
 
     printf("xres: %d, yres: %d\n", xRes, yRes);
@@ -93,7 +121,7 @@ int main() {
     printf("bits per pixel: %d\n", bits_per_pixel);
 
 
-    // draw_boundingbox(x_start, y_start, x_len, y_len);
+    draw_boundingbox(x_start, y_start, x_len, y_len);
 
 
     return 0;
