6b705e0ed468930fa05b0fc6ca04db4f3677df01
John Privitera <johnpriv@live.com>
2022-03-26 14:43:54 -0400

    Remove timestamp from structs, add stub Makefile and cv_main.h

--- computer-vision/cv_main.cpp
+++ computer-vision/cv_main.cpp
@@ -1,43 +1,56 @@
+#include "cv_main.h"
 #include <iostream>
-#include "stdint.h"
-#include <ctime>
+// TODO: Rename "cv_main.cpp" and "cv_main.h" later on
+// TODO: Update Makefile with the new name
+// TODO: Remove <iostream> after cout testing is done
 
-#define MAX_BBOXES 5
+int ImportFrame()
+{
+  // TODO: Implement ImportFrame()
+  std::cout << "Importing frame\n";
 
-using namespace std;
+  // TODO: Add error Handling
+  return 0;
+}
 
-struct coordinate_data {
-  uint16_t x_coord;
-  uint16_t y_coord;
-  uint16_t x_len;
-  uint16_t y_len;
-};
+int GenerateBBoxes(int frame)
+{
+  // TODO: Implement GenerateBBoxes()
+  std::cout << "Generate bounding boxes\n";
 
-struct cv_data {
-  int num_bbox = 0;
-  time_t t = time(0);
-  struct coordinate_data box_data[MAX_BBOXES];
-};
+  // TODO: Add error Handling
+  return 0;
+}
 
-int main() {
+int TransmitStruct(cv_data data_to_send)
+{
+  // TODO: Implement TransmitStruct()
+  std::cout << "Sending cv_data out\n";
+
+  // TODO: Add error Handling
+  return 0;
+}
+
+int main()
+{
   struct cv_data cv_data_current;
   struct coordinate_data box1;
   struct coordinate_data box2;
   // Set box 1
-  box1.x_coord = 15; // x coordinate
+  box1.x_coord = 15;  // x coordinate
   box1.y_coord = 100; // y coordinate
-  box1.x_len = 20; // x length
-  box1.y_len = 50; // y length
+  box1.x_len = 20;    // x length
+  box1.y_len = 50;    // y length
   // Set box 2
-  box2.x_coord = 15; // x coordinate
+  box2.x_coord = 15;  // x coordinate
   box2.y_coord = 100; // y coordinate
-  box2.x_len = 20; // x length
-  box2.y_len = 50; // y length
-  
+  box2.x_len = 20;    // x length
+  box2.y_len = 50;    // y length
+
   cv_data_current.num_bbox = 2;
   cv_data_current.box_data[0] = box1;
   cv_data_current.box_data[1] = box2;
-  
-  cout << cv_data_current.box_data[0].x_coord<<endl;
+
+  std::cout << cv_data_current.box_data[0].x_coord << std::endl;
   return 0;
 }
--- computer-vision/cv_main.h
+++ computer-vision/cv_main.h
@@ -1,25 +1,12 @@
 #ifndef __CV_MAIN_H__
 #define __CV_MAIN_H__
+#include "cv_structs.h"
 
-#include <iostream>
-#include "stdint.h"
-#include <ctime>
+// TODO: Rename "cv_main.cpp" and "cv_main.h" later on
+// Having a header for functions seems like it could be nice, but is it needed?
 
-#define MAX_BBOXES 5
-
-// holds the data needed to draw an individual bounding box
-struct coordinate_data {
-  uint16_t x_coord;
-  uint16_t y_coord;
-  uint16_t x_len;
-  uint16_t y_len;
-};
-
-// struct that will be transmitted and contains all necessary CV info
-struct cv_data {
-  int num_bbox = 0; // number of bounding boxes
-  time_t t = time(0); // timestamp (will be provided by camera team)
-  struct coordinate_data box_data[MAX_BBOXES]; // collection of bounding boxes
-};
+int ImportFrame();
+int GenerateBBoxes(int frame); // Change datatype from int to whatever the frame is
+int TransmitStruct(cv_data data_to_send);
 
 #endif
