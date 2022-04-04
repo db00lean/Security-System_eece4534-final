6a37db3af9f9735ada13de98f42a4d9064e17428
Connor Northway <32226655+cnorthway@users.noreply.github.com>
2022-04-03 20:35:30 -0400

    first somewhat functional C example
    use gdb to prove the memory is filled with a pgm
    
    references issue(s): #37

---
+++ HDMI/rtsp-rx/gstreamer-rx.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+
+#include <gst/gst.h>
+#include <gst/app/gstappsink.h>
+
+int
+main (int argc, char *argv[])
+{
+  GstElement *pipeline;
+  GstBus *bus;
+  //GstMessage *msg;
+  GstStateChangeReturn ret;
+
+  /* Initialize GStreamer */
+  gst_init (&argc, &argv);
+
+// works, buuut PGM. has a header
+  pipeline = gst_parse_launch("-e -v rtspsrc location=\"rtsp://rtsp.stream/pattern\" ! rtph264depay ! h264parse !  decodebin ! videoconvert ! avenc_ppm ! appsink name=sink", NULL);
+
+  gst_element_set_state(pipeline, GST_STATE_PLAYING);
+
+  bus = gst_element_get_bus(pipeline);
+
+  GstElement *sink = gst_bin_get_by_name(GST_BIN(pipeline), "sink");
+  if (!sink) {
+      printf("sink is NULL\n");
+      exit(1);
+  }
+
+  GstAppSink *appsink = GST_APP_SINK(sink);
+  if (!appsink) {
+      printf("appsink is NULL\n");
+      exit(1);
+  }
+
+  /* Start playing */
+  ret = gst_element_set_state (pipeline, GST_STATE_PLAYING);
+  if (ret == GST_STATE_CHANGE_FAILURE) {
+    g_printerr ("Unable to set the pipeline to the playing state.\n");
+    gst_object_unref (pipeline);
+    return -1;
+  }
+
+
+  GstSample *sample = gst_app_sink_pull_sample(appsink);
+  if (!sample) {
+      printf("sample is NULL\n");
+      exit(1);
+  }
+
+
+  // what is this doing??
+  GstBuffer *buffer = gst_sample_get_buffer(sample);
+  GstMapInfo map;
+
+  gst_buffer_map(buffer, &map, GST_MAP_READ);
+
+  // Allocate appropriate buffer to store compressed image
+  char *pRet = malloc(map.size);
+  // Copy image
+  memmove(pRet, map.data, map.size);
+
+  free(pRet);
+
+//  /* Wait until error or EOS */
+//  bus = gst_element_get_bus (pipeline);
+//  msg =
+//      gst_bus_timed_pop_filtered (bus, GST_CLOCK_TIME_NONE,
+//      GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+//
+//  /* Parse message */
+//  if (msg != NULL) {
+//    GError *err;
+//    gchar *debug_info;
+//
+//    switch (GST_MESSAGE_TYPE (msg)) {
+//      case GST_MESSAGE_ERROR:
+//        gst_message_parse_error (msg, &err, &debug_info);
+//        g_printerr ("Error received from element %s: %s\n",
+//            GST_OBJECT_NAME (msg->src), err->message);
+//        g_printerr ("Debugging information: %s\n",
+//            debug_info ? debug_info : "none");
+//        g_clear_error (&err);
+//        g_free (debug_info);
+//        break;
+//      case GST_MESSAGE_EOS:
+//        g_print ("End-Of-Stream reached.\n");
+//        break;
+//      default:
+//        /* We should not reach here because we only asked for ERRORs and EOS */
+//        g_printerr ("Unexpected message received.\n");
+//        break;
+//    }
+//    gst_message_unref (msg);
+//  }
+
+  printf("done\n");
+
+  /* Free resources */
+  gst_object_unref (bus);
+  gst_element_set_state (pipeline, GST_STATE_NULL);
+  gst_object_unref (pipeline);
+  return 0;
+}
+
