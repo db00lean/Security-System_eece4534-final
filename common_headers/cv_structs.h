a55b7d2eaaeaaff70c7433d3a1d7e20a76579c9b
Benjamin Palmer <palmer.b@husky.neu.edu>
2022-04-04 14:05:24 -0400

    Restructure code into camera-module (client) and base-station directories to contain all application code. Anything that runs on the system should be added to those directories.

--- common_headers/cv_structs.h
+++ common_headers/cv_structs.h
@@ -1,14 +1,12 @@
 #ifndef __CV_STRUCT_H__
 #define __CV_STRUCT_H__
 
-#include <iostream>
 #include <stdint.h>
 
 #define MAX_B_BOXES 8
 
 // holds the data needed to draw an individual bounding box
-struct coordinate_data
-{
+struct coordinate_data {
   uint16_t x_coord;
   uint16_t y_coord;
   uint16_t x_len;
@@ -16,9 +14,8 @@ struct coordinate_data
 };
 
 // struct that will be transmitted and contains all necessary CV info
-struct cv_data
-{
-  int num_bbox = 0;                             // number of bounding boxes
+struct cv_data {
+  int num_bbox;                                 // number of bounding boxes
   struct coordinate_data box_data[MAX_B_BOXES]; // collection of bounding boxes
 };
 
