f9aaf90cad722008b0495eaf9bf6a04aed05d167
mattdowning314 <downing.matthew123@gmail.com>
2022-04-07 20:39:44 -0400

    #52 added in functions for circle, filled and outline, and filled in rectangle, all untested and possible errors occur

--- base-station/HDMI/src/draw_bounding_box.c
+++ base-station/HDMI/src/draw_bounding_box.c
@@ -8,6 +8,8 @@ Writes box pixels to frame buffer /dev/fb0
 
 #include <stdio.h>
 #include <stdint.h>
+#include <math.h>
+#include <stdlib.h>
 
 void draw_boundingbox(int x_start, int y_start, int x_len, int y_len, unsigned int color) {
     //origin is top left of rectangle (x_start, y_start)
@@ -44,13 +46,57 @@ void draw_boundingbox(int x_start, int y_start, int x_len, int y_len, unsigned i
 }
 
 void draw_circle_outline(int x_center, int y_center, int radius, unsigned int color) {
-
+    //first half of outline
+    for (int x = x_center - radius; x <= x_center + radius; x++) {
+        int y = y_center + (int)sqrt(pow(radius, 2) - pow((x-x_center), 2));
+        draw_pixel(x, y, color);
+    }
+    //second half of outline
+    for (int x = x_center - radius; x <= x_center + radius; x++) {
+        int y = y_center - (int)sqrt(pow(radius, 2) - pow((x-x_center), 2));
+        draw_pixel(x, y, color);
+    }
 }
 
 void draw_circle_filled(int x_center, int y_center, int radius, unsigned int color) {
+    //store current state in array
+    int state[radius*2][radius*2];
+    for (int i = 0; i<(radius*2); i++) {
+        for (int j = 0; j<(radius*2); j++) {
+            state[i][j] = 0;
+        }
+    }
 
+    //first half of outline
+    for (int x = x_center - radius; x <= x_center + radius; x++) {
+        int y = y_center + (int)sqrt(pow(radius, 2) - pow((x-x_center), 2));
+        draw_pixel(x, y, color);
+        state[x-radius][y-radius] = 1;
+    }
+    //second half of outline
+    for (int x = x_center - radius; x <= x_center + radius; x++) {
+        int y = y_center - (int)sqrt(pow(radius, 2) - pow((x-x_center), 2));
+        draw_pixel(x, y, color);
+        state[x-radius][y-radius] = 1;
+    }
+    //fill in circle
+    for (int x = x_center - radius; x <= x_center + radius; x++) {
+        for (int y = y_center - radius; y<= y_center + radius; y++) {
+            if (state[x][y] == 1) {
+                y++;
+                while(state[x][y] == 0) {
+                    state[x][y] = 1;
+                    y++;
+                }
+            }
+        }
+    }
 }
 
 void draw_rectangle_filled(int x_start, int y_start, int x_len, int y_len, unsigned int color) {
-    
+    for (int x = x_start; x<x_len; x++) {
+        for (int y = y_start; y<y_len; y++) {
+            draw_pixel(x, y, color);
+        }
+    }
 }
