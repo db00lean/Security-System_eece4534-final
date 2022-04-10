93b94517c564dce833574f572a208667eb5dabc7
JosephBerman <berman.jos@northeastern.edu>
2022-04-10 14:45:26 -0400

    Page flip compiles, need to test it out fully now

--- base-station/HDMI/display/DRM/TestingDir/DRM_user_test.c
+++ base-station/HDMI/display/DRM/TestingDir/DRM_user_test.c
@@ -12,6 +12,17 @@
 #include <drm_fourcc.h>
 
 
+//Struct to hold frame contents
+struct buf_context{
+	uint32_t width;
+	uint32_t height;
+	uint32_t pitch;
+	uint32_t size;
+	uint32_t handle;
+	void *bufmap;
+	uint32_t fb;
+    
+};
 
 
 #define PIXEL(x, y) ((y * IMG_W * 3) + (x * 3))
@@ -28,16 +39,10 @@ drmModeEncoder *encode;
 drmModeCrtc *crtc;
 drmModeFB *fb;
 
-//Pointer to memory mapped region for writing to card
-void *map;
 
-struct drm_mode_create_dumb crereq;
 
-// struct to create memory mapping for dumb buffer
-struct drm_mode_map_dumb mreq;
+//Pointer to memory mapped region for writing to card void *map;
 
-// struct to destroy dumb buffer
-struct drm_mode_destroy_dumb dreq;
 //Defining constants for colors according to default pixel format -- 32 bit word with transparency, red, green, and blue values
 uint32_t const red = (0xff << 16);
 uint32_t const green = (0xff << 8);
@@ -94,6 +99,88 @@ int drm_init(int fd)
 
     return 0;
 }
+
+int makeFB(int fd, struct buf_context *buf){
+// 32 bit memory location to store address of framebuffer
+
+
+    int ret;
+    // struct to create dumb buffer
+
+
+    struct drm_mode_create_dumb crereq;
+
+    // struct to create memory mapping for dumb buffer
+    struct drm_mode_map_dumb mreq;
+
+    // struct to destroy dumb buffer
+    struct drm_mode_destroy_dumb dreq;
+
+    // clear crereq before setting members
+    memset(&crereq, 0, sizeof(crereq));
+
+    // set members of crereq based on members of "drmModeModeInfo" obtained in drm_init()
+    crereq.height = mode->vdisplay;
+    crereq.width = mode->hdisplay;
+    crereq.bpp = 32;
+
+    // create dumb DRM based on crereq members -- "handle, pitch, size will be returned", members of crereq
+    ret = drmIoctl(fd, DRM_IOCTL_MODE_CREATE_DUMB, &crereq);
+
+    if (ret)
+    {
+        printf("Failed to make dumb buffer");
+        return -1;
+    }
+    // create DRM FB using information within crereq, populated by drmIoctl call above
+    ret = drmModeAddFB(fd, (uint32_t)crereq.width, (uint32_t)crereq.height, 24,
+                       crereq.bpp, crereq.pitch, crereq.handle, &buf->fb);
+
+
+    if (ret)
+    {
+        printf("Failed to create DRM buffer\n");
+        printf("ret: %d\n", ret);
+        return -3;
+    }
+
+    buf->pitch = crereq.pitch;
+    buf->handle = crereq.handle;
+    buf->size = crereq.size;
+
+    // Clear mreq
+    memset(&mreq, 0, sizeof(mreq));
+
+    // Set memory mapping handle equal to the handle of the dumb fb just created
+    mreq.handle = buf->handle;
+    // Map dumb buffer based on mreq.handle
+    // This Ioctl call populates mreq.offset, used in mmap call below
+    ret = drmIoctl(fd, DRM_IOCTL_MODE_MAP_DUMB, &mreq);
+
+    if (ret)
+    {
+        printf("Failed to map dumb buffer");
+        return -2;
+    }
+
+ 
+
+    // Map memory region for DRM framebuffer using size and mapped offset of dumbbuffer
+    buf->bufmap = mmap(0, buf->size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, mreq.offset);
+
+    if (buf->bufmap == MAP_FAILED)
+    {
+        printf("map failed");
+        return -3;
+    }
+
+    memset(buf->bufmap, 0, buf->size);
+
+   
+
+    return 0;
+}
+/*
 void *drm_map(int fd)
 {
     // 32 bit memory location to store address of framebuffer
@@ -123,12 +210,16 @@ void *drm_map(int fd)
     ret = drmModeAddFB(fd, (uint32_t)crereq.width, (uint32_t)crereq.height, 24,
                        crereq.bpp, crereq.pitch, crereq.handle, &fb);
 
+
     if (ret)
     {
         printf("Failed to create DRM buffer\n");
         printf("ret: %d\n", ret);
         return MAP_FAILED;
     }
+
+
+
 
     // Clear mreq
     memset(&mreq, 0, sizeof(mreq));
@@ -164,10 +255,12 @@ void *drm_map(int fd)
     }
     return map;
 }
+*/
+/*
 void drm_unmap()
 {
-    munmap(map, crereq.size);
-}
+    munmap(map, buf.size);
+}*/
 void print_info()
 {
     //Printing information from libdrm structs, filled in drm_init()
@@ -206,11 +299,11 @@ int drm_close()
 
     return 0;
 }
-void draw_pixel(int x, int y, uint32_t ARGB)
+void draw_pixel(int x, int y, uint32_t ARGB, void* bufmap)
 {
     //Local pointer to point to memory mapped display region
     uint32_t *pixelPtr;
-    pixelPtr = (uint32_t *)(map);
+    pixelPtr = (uint32_t *)(bufmap);
 
     //Advance pixelPtr to correct row
     pixelPtr += mode->hdisplay * y;
@@ -219,7 +312,7 @@ void draw_pixel(int x, int y, uint32_t ARGB)
 
     *pixelPtr = ARGB;
 }
-void demo()
+void demo(struct buf_context buf)
 {
     int y, x;
     //Loop to iterate through rows
@@ -231,18 +324,52 @@ void demo()
             //Draw top 3rd of screen red
             if (y < (mode->vdisplay / 3))
             {
-                draw_pixel(x, y, colors[0]);
+                draw_pixel(x, y, colors[0], buf.bufmap);
             }
             //Draw middle 3rd of screen blue
             else if (y < (mode->vdisplay * 2 / 3))
             {
-                draw_pixel(x, y, colors[1]);
+                draw_pixel(x, y, colors[1],  buf.bufmap);
             }
             //Draw bottom 3rd of screen green
             else
             {
-                draw_pixel(x, y, colors[2]);
+                draw_pixel(x, y, colors[2],  buf.bufmap);
             }
         }
     }
+}
+
+
+void pageFlip(int fd, struct buf_context* bufs){
+
+
+
+    drmSetMaster(fd);
+
+    // clear crtc
+    drmModeSetCrtc(fd, crtc->crtc_id, 0, 0, 0, NULL, 0, NULL);
+
+    drmModeSetCrtc(fd, crtc->crtc_id, bufs[0].fb, 0, 0, &conn->connector_id, 1, mode);
+
+    int i =0;
+    while(1){
+      
+
+        getchar();
+        printf("Changing to FB: %d", i);
+        drmModePageFlip(fd, crtc->crtc_id, bufs[i].fb, DRM_MODE_PAGE_FLIP_EVENT, NULL );
+        
+        if(i == 1){
+            i = 0;
+        }
+        else{
+            i = 1;
+        }
+    }
+
+
+    drmDropMaster(fd);
+
+
 }
--- base-station/HDMI/display/DRM/TestingDir/main_drm_test.c
+++ base-station/HDMI/display/DRM/TestingDir/main_drm_test.c
@@ -1,14 +1,19 @@
-#include "DRM_user_test.h"
+#include "./DRM_user_test.h"
 
 int main(){
 
     int fd, ret;
+    struct buf_context bufs[2];
     fd = drm_open();
     drm_init(fd);
-    map = drm_map(fd);
+    makeFB(fd, &bufs[0]);
+
+    makeFB(fd, &bufs[1]);
+    //map = drm_map(fd);
 
     print_info();
 
-    demo();
-    getchar();
+    demo(bufs[0]);
+   // getchar();
+   pageFlip(fd, &bufs[0]);
 }
