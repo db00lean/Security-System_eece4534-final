2b24f449823dea6af318ebfa78a336b130adb1f8
John Craffey <craffey.john@gmail.com>
2022-04-14 21:43:45 -0400

    gui can show detection of 2 boxes on screen at same time #91

--- base-station/aggregate_detect.c
+++ base-station/aggregate_detect.c
@@ -30,21 +30,27 @@ int detect(struct coordinate_data rzone_data, struct coordinate_data box_data) {
 // Input: bounding box input in system_management.h
 void aggregate_detect(struct camera_module *cam) {
   // cam.detection = detect(cam.forbiddenZone, cam.cvMetadata.box_data[0]);
-  cam->detection =
-      detect(cam->forbiddenZone, cam->cvMetadata.box_data[0]);
+  int detection = 0;
+  for (int ii = 0; ii < cam->cvMetadata.num_bbox; ii++) {
+    detection |= detect(cam->forbiddenZone, cam->cvMetadata.box_data[ii]);
+    printf("\nMetadata for camera %d\n", cam->cameraNumber);
+    printf("number of bbox: %d\n", cam->cvMetadata.num_bbox);
+    printf("detection box %d: %d\n", ii, detection);
+  }
+  cam->detection = detection;
 
-  printf("\nMetadata for camera %d\n", cam->cameraNumber);
-  printf("number of bbox: %d\n", cam->cvMetadata.num_bbox);
-  // printf("timestamp: %ld\n",cam.cvMetadata.t);
-  printf("box 0 xcoord: %d\n", cam->cvMetadata.box_data[0].x_coord);
-  printf("box 0 ycoord: %d\n", cam->cvMetadata.box_data[0].y_coord);
-  printf("box 0 x len: %d\n", cam->cvMetadata.box_data[0].x_len);
-  printf("box 0 y len: %d\n", cam->cvMetadata.box_data[0].y_len);
-  printf("box 1 xcoord: %d\n", cam->cvMetadata.box_data[1].x_coord);
-  printf("box 1 y coord: %d\n", cam->cvMetadata.box_data[1].y_coord);
-  printf("box 1 x len: %d\n", cam->cvMetadata.box_data[1].x_len);
-  printf("box 1 y len: %d\n", cam->cvMetadata.box_data[1].y_len);
-  printf("detection: %d\n", cam->detection);
+  // printf("\nMetadata for camera %d\n", cam->cameraNumber);
+  // printf("number of bbox: %d\n", cam->cvMetadata.num_bbox);
+  // // printf("timestamp: %ld\n",cam.cvMetadata.t);
+  // printf("box 0 xcoord: %d\n", cam->cvMetadata.box_data[0].x_coord);
+  // printf("box 0 ycoord: %d\n", cam->cvMetadata.box_data[0].y_coord);
+  // printf("box 0 x len: %d\n", cam->cvMetadata.box_data[0].x_len);
+  // printf("box 0 y len: %d\n", cam->cvMetadata.box_data[0].y_len);
+  // printf("box 1 xcoord: %d\n", cam->cvMetadata.box_data[1].x_coord);
+  // printf("box 1 y coord: %d\n", cam->cvMetadata.box_data[1].y_coord);
+  // printf("box 1 x len: %d\n", cam->cvMetadata.box_data[1].x_len);
+  // printf("box 1 y len: %d\n", cam->cvMetadata.box_data[1].y_len);
+  // printf("detection: %d\n", cam->detection);
   // printf("Forbidden zone: %d, %d, %d, %d\n", cam.forbiddenZone.x_coord,
   // cam.forbiddenZone.y_coord, cam.forbiddenZone.x_len,
   // cam.forbiddenZone.y_len);
