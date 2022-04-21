2c34c24c9a31d0355a6993b76b7a74a134683645
JosephBerman <berman.jos@northeastern.edu>
2022-04-21 16:32:54 -0400

    pageFlip is fully abstracted

--- base-station/HDMI/display/DRM/TestingDir/main_drm_test.c
+++ base-station/HDMI/display/DRM/TestingDir/main_drm_test.c
@@ -1,9 +1,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "./DRM_user_test.h"
+#include "DRM_user_test.h"
 
 
+#define SIXTYFPSMICROSECONDS 166667
 
 int main()
 {
@@ -14,50 +15,24 @@ int main()
 
     int fd, ret;
 
-    struct buf_context *bufs[BUFF_AMOUNTS];
-
     fd = drm_open();
     drmSetMaster(fd);
 
-    int i = 0;
-    for (i = 0; i < BUFF_AMOUNTS; i++) {
-        bufs[i] = malloc(sizeof(struct buf_context));
+    drm_init(fd);
 
 
-        bufs[i]->fd = fd;
+    demo();
 
+    changeActiveBuffer();
 
-        drm_init(fd);
-        bufs[i]->map = drm_map(bufs[i]->fd, bufs[i], i);
-        //print_info();
-
-    }
-
-
-    demo(bufs[0]);
-
-
-
-
-
-    demo2( bufs[1]);
+    demo2();
 
    
 
-    i =0;
+    int i = 0;
     while(1){
-        if(i == 0){
-            demo(bufs[0]);
-             pageFlip(fd,  bufs[0]);
-             i = 1;
-        }
-        else{
-            demo2( bufs[1]);
-             pageFlip(fd,  bufs[1]);
-             i = 0;
-        }
-     usleep(SIXTYFPSMICROSECONDS);
-
+        pageFlip();
+        usleep(SIXTYFPSMICROSECONDS);
     }
 
     
@@ -72,3 +47,4 @@ int main()
     drm_unmap(bufs[0]);
     drm_unmap(bufs[1]);
 }
+
