b5425f5a9cba2cb74759ea10d54b70bece4a6ba2
John Privitera <johnpriv@live.com>
2022-04-26 14:03:18 -0400

    Add cv_t thread to systerm_management_client.cpp

--- camera-module/system_management_client.cpp
+++ camera-module/system_management_client.cpp
@@ -25,47 +25,49 @@
 std::queue<struct cv_data> cv_data_q;
 pthread_mutex_t mutex;
 
-struct stream_args {
+struct stream_args
+{
   int argc;
   char **argv;
 };
 
 // stream thread main function
-void *stream(void *thread_args) {
+void *stream(void *thread_args)
+{
   struct stream_args *args = (struct stream_args *)thread_args;
   stream_server(args->argc, args->argv);
   return 0;
 }
-// computer vision main function
-void *cv_t(void *thread_args) {
-  GetBBoxesFromFrame();
 
-#ifdef __CV_STRUCT_H__
-  // Code for when sysman has access to CV code
-  // TODO: Delete the #ifdef/#endif after CV is integrated to allow this code to
-  // run
-  /*bool do_run_cv = true;
-  while (do_run_cv) {
+// computer vision main function
+void *cv_t(void *thread_args)
+{
+  bool do_run_cv = true;
+  cv_data current_data;
+
+  while (do_run_cv)
+  {
     // TODO: Add "gstream camera_stream" as argument to GetBBoxesFromFrame (or
     // whatever the gstream type is)
-    pthread_mutex_lock(&mutex;);   // Lock
-    cv_data_q.push(GetBBoxesFromFrame());
+    current_data = GetBBoxesFromFrame();
+    pthread_mutex_lock(&mutex;); // Lock
+    cv_data_q.push(current_data);
     pthread_mutex_unlock(&mutex;); // Unlock
-  }*/
-#endif
+  }
 
   return NULL;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
   // ### init ###
   // networking stuff
   // TODO: find cam_id
-  const char* port = "55000"; // Statically defined for now
-  const char* address = "127.0.0.1";
+  const char *port = "55000"; // Statically defined for now
+  const char *address = "127.0.0.1";
   int cam_id;
-  struct client* c = new_client(port, address);
+  struct client *c = new_client(port, address);
 
   // ### kick off threads ###
 
@@ -76,16 +78,15 @@ int main(int argc, char *argv[]) {
 
   // CV
   pthread_t cv_main_thread;
-  pthread_mutex_init(&mutex,NULL);
+  pthread_mutex_init(&mutex, NULL);
   pthread_create(&cv_main_thread, NULL, cv_t, NULL);
   struct cv_data out;
 
-  pthread_mutex_lock(&mutex);   // Lock
+  pthread_mutex_lock(&mutex); // Lock
   out = cv_data_q.front();
   cv_data_q.pop();
-  pthread_mutex_unlock(&mutex);  // Unlock
-  send_msg(c->requester, cam_id, CV_DATA, (void*)&out, sizeof(struct cv_data)+sizeof(struct coordinate_data));
-
+  pthread_mutex_unlock(&mutex); // Unlock
+  send_msg(c->requester, cam_id, CV_DATA, (void *)&out, sizeof(struct cv_data) + sizeof(struct coordinate_data));
 
   // ### cleanup ###
   pthread_join(cv_main_thread, NULL);
