7834a57cd41fa11e6d4f8afb82184caf64ec40f2
John Privitera <johnpriv@live.com>
2022-05-01 19:47:15 -0400

    Increase image size to 1920x1080p from stream

--- camera-module/cv_main.cpp
+++ camera-module/cv_main.cpp
@@ -6,8 +6,10 @@
 #define NUM_CHANNELS 3
 #define ARRAY_DIM 3
 
-#define IMAGE_HEIGHT 100
-#define IMAGE_WIDTH 100
+// Can probably lower these to height=540, width=960 and scale bbox values up by 2 at end
+// Will look into this after streaming is integrated
+#define IMAGE_HEIGHT 1080
+#define IMAGE_WIDTH 1920
 
 #if DEBUG_MODE_CV
 #include <iostream>
@@ -53,24 +55,23 @@ cv::Mat ImportFrame()
 }
 #endif
 
-
-cv::Mat StreamFrame(struct camera_rx* cam)
+cv::Mat StreamFrame(struct camera_rx *cam)
 {
   // struct image * img = get_frame(cam, IMGENC_BGR, IMAGE_WIDTH, IMAGE_HEIGHT);
 
-  struct image * img = create_image(IMGENC_BGR, IMAGE_WIDTH, IMAGE_HEIGHT);
+  struct image *img = create_image(IMGENC_BGR, IMAGE_WIDTH, IMAGE_HEIGHT);
 
   // convert image to cv Mat
-  //const int sizes[3] = {img->height, img->width, NUM_CHANNELS};
-  //const size_t steps[2] = {(img->width * sizeof(char)), sizeof(char)};
-  //cv::Mat cv_frame = cv::Mat(ARRAY_DIM, &sizes, CV_8UC3, (void *) img->buf, &steps);
+  // const int sizes[3] = {img->height, img->width, NUM_CHANNELS};
+  // const size_t steps[2] = {(img->width * sizeof(char)), sizeof(char)};
+  // cv::Mat cv_frame = cv::Mat(ARRAY_DIM, &sizes, CV_8UC3, (void *) img->buf, &steps);
   cv::Mat frame = cv::Mat(sizeof(img->buf), 1, CV_8UC3, img->buf).clone();
   cv::Mat cv_frame = frame.reshape(0, IMAGE_HEIGHT);
 
   // write to an image file to test if this works
   bool result = cv::imwrite("streamed_frame_test.png", cv_frame);
- 
-  if(!result)
+
+  if (!result)
   {
     std::cout << "Unable to write test image\n";
   }
@@ -245,7 +246,7 @@ int cv_main()
   }
 #endif
 
-  //cleanup_rx_camera(cam);
-  //free_rx_camera(cam);
+  // cleanup_rx_camera(cam);
+  // free_rx_camera(cam);
   return 0;
 }
