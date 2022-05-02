51bb0952d27568634f280b420bb3050f7029bfa4
Tom Doyle <doyle.t@northeastern.edu>
2022-05-02 19:27:59 -0400

    adding changes from Thursday testing, rebuilding libdrm not working

---
+++ base-station/HDMI/display/DRM/TestingDir/mem_copy_test.c
@@ -0,0 +1,50 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "DRM_user_test.h"
+
+
+#define SIXTYFPSMICROSECONDS 166667
+
+int main()
+{
+    //struct buf_context *myBuf0 = malloc(sizeof(struct buf_context));
+    //struct buf_context *myBuf1 = malloc(sizeof(struct buf_context));
+
+    printf("inside main\n");
+
+    int fd, ret;
+
+    fd = drm_open();
+    drmSetMaster(fd);
+
+    drm_init(fd);
+
+
+    demo();
+
+    changeActiveBuffer();
+
+    demo2();
+
+   
+
+    int i = 0;
+    while(1){
+        pageFlip();
+        usleep(SIXTYFPSMICROSECONDS);
+    }
+
+    
+
+    // pageFlip(myBuf1);
+
+
+
+    drm_close();
+    drmDropMaster(fd);
+
+    drm_unmap(bufs[0]);
+    drm_unmap(bufs[1]);
+}
+
